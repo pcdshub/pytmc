d59b9a139dc70bec22ba0db09831b1f3e72c9e44
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2018-01-17 13:00:03 -0800

    DEV/TST: adding first live testsw

---
+++ pytpy/xml_obj.py
@@ -0,0 +1,14 @@
+"""
+xml_obj.py
+
+This file contains the objects for representing the essential TwinCAT
+datastructures found in xml (tmc) formatted file.
+"""
+
+
+class Symbol:
+    pass
+
+class DataType:
+    pass
+
---
+++ tests/test_xml_obj.py
@@ -0,0 +1,11 @@
+import pytest
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+def test_Symbol_instantiation():
+    pass
+
+def test_DataType_instantiation():
+    pass
