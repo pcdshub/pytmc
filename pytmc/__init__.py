290de968286417c6ed3bb89656dedc1bf2a0450c
n-wbrown <n-wbrown@users.noreply.github.com>
2018-10-09 16:53:41 -0700

    TST: tmc_render removed

--- pytmc/__init__.py
+++ pytmc/__init__.py
@@ -4,8 +4,6 @@ logger = logging.getLogger(__name__)
 
 from .xml_obj import Symbol, DataType, SubItem
 from .xml_collector import TmcFile
-from .tmc_render import (DbRenderAgent, SingleRecordData, TmcExplorer,
-            FullRender, SingleProtoData, ProtoRenderAgent)
 
 from ._version import get_versions
 __version__ = get_versions()['version']
