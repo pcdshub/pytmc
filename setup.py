46ec9727aaea1f5accbcea072ba74f0da5cccda9
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2019-02-13 15:22:10 -0800

    DEV: commandline interface improved, now 'pytmc' is main command

--- setup.py
+++ setup.py
@@ -2,13 +2,20 @@ import versioneer
 from setuptools import (setup, find_packages)
 
 
-setup(name     = 'pytmc',
-      version  = versioneer.get_version(),
-      cmdclass = versioneer.get_cmdclass(),
-      author   = 'SLAC National Accelerator Laboratory',
-      license='BSD',
-      packages    = find_packages(),
-      description = 'Generate Epics DB records from TwinCAT .tmc files',
-      scripts = ['bin/xmltranslate','bin/makerecord'],
-      include_package_data = True,
+setup(
+    name     = 'pytmc',
+    version  = versioneer.get_version(),
+    cmdclass = versioneer.get_cmdclass(),
+    author   = 'SLAC National Accelerator Laboratory',
+    license='BSD',
+    packages    = find_packages(),
+    description = 'Generate Epics DB records from TwinCAT .tmc files',
+    #scripts = ['bin/xmltranslate','bin/makerecord'],
+    entry_points = {
+        'console_scripts': [
+            'pytmc = pytmc.bin.makerecord:main',
+            'xmltranslate = pytmc.bin.xmltranslate:main',
+        ]
+    },
+    include_package_data = True,
 )
