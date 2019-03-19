d6b6a9c0556a8d4daaadb1dda69ffb8c112eccb8
n-wbrown <nwbrown@slac.stanford.edu>
2019-03-19 10:52:36 -0700

    STY: pep8 fixes

--- pytmc/bin/makerecord.py
+++ pytmc/bin/makerecord.py
@@ -1,54 +1,59 @@
+"""
+"pytmc" is a command line utility for generating epics records files from
+TwinCAT3 .tmc files. This program is designed to work in conjunction with
+ESSS' m-epics-twincat-ads driver.
+"""
+
 import logging
-logger = logging.getLogger(__name__)
+import argparse
 
 import pytmc
-import argparse
-from .. import TmcFile
+
+
+logger = logging.getLogger(__name__)
+description = __doc__
 
 
 def main():
-    description = """\
-    "pytmc" is a command line utility for generating epics records files from
-    TwinCAT3 .tmc files. This program is designed to work in conjunction with
-    ESSS' m-epics-twincat-ads driver."""
-    
     parser = argparse.ArgumentParser(
-        description = description,
-        formatter_class = argparse.RawTextHelpFormatter
+        description=description,
+        formatter_class=argparse.RawTextHelpFormatter
     )
-    
+
     parser.add_argument(
-        'tmc_file', metavar="INPUT", type=str, help='Path to interpreted .tmc file'
+        'tmc_file', metavar="INPUT", type=str,
+        help='Path to interpreted .tmc file'
     )
-    
+
     parser.add_argument(
-        'record_file', metavar="OUTPUT", type=str, help='Path to output .db file'
+        'record_file', metavar="OUTPUT", type=str,
+        help='Path to output .db file'
     )
 
     parser.add_argument(
         '--log',
         '-l',
         metavar="LOG_LEVEL",
-        default=30, #WARN level messages
+        default=30,  # WARN level messages
         type=int,
         help='Python numeric logging level (e.g. 10 for DEBUG, 20 for INFO'
     )
-    
+
     args = parser.parse_args()
     pytmc_logger = logging.getLogger('pytmc')
     pytmc_logger.setLevel(args.log)
-    tmc_file = open(args.tmc_file,'r')
+    tmc_file = open(args.tmc_file, 'r')
     tmc_obj = pytmc.TmcFile(tmc_file)
     tmc_obj.create_chains()
     tmc_obj.isolate_chains()
     tmc_obj.create_packages()
     tmc_obj.configure_packages()
     db_string = tmc_obj.render()
-    record_file = open(args.record_file,'w')
+    record_file = open(args.record_file, 'w')
     record_file.write(db_string)
     tmc_file.close()
     record_file.close()
 
+
 if __name__ == '__main__':
     main()
-
--- pytmc/bin/xmltranslate.py
+++ pytmc/bin/xmltranslate.py
@@ -1,15 +1,10 @@
-import xml.etree.ElementTree as ET
-import textwrap
-import sys
-import argparse
-
-description = """Tool for making XML-formatted files human-readable
+"""Tool for making XML-formatted files human-readable
 
 Output uses the following rules:
 For a line of xml that appears as the following:
 <tag attr=qual...> text </tag> tail
 
-Note that tail takes precedence over text. 
+Note that tail takes precedence over text.
 
 This program will output the following:
 {attrs:qual...} tag text tail
@@ -20,35 +15,40 @@ This tool was created for exploring .tpy files but is well suited to reading
 any xml formatted file.
 """
 
+import xml.etree.ElementTree as ET
+import textwrap
+import argparse
+
 
 parser = argparse.ArgumentParser(
-    description = description, 
-    formatter_class = argparse.RawTextHelpFormatter
+    description=__doc__,
+    formatter_class=argparse.RawTextHelpFormatter
 )
 parser.add_argument(
     'input_file', metavar="INPUT", type=str, help='input file'
 )
 
 parser.add_argument(
-    '-d', '--depth', type=int, help='Recursive limit for exploring the file', 
-    default = 7
+    '-d', '--depth', type=int, help='Recursive limit for exploring the file',
+    default=7
 )
 
 parser.add_argument(
     '-i', '--indent_size', metavar="INDENT",
-    type=int, help='Indent size for output formatting', 
-    default = 4
+    type=int, help='Indent size for output formatting',
+    default=4
 )
 
 args = parser.parse_args()
 
+
 def recursive(branch, level=1, indent_size=4, indent=0):
     if branch == None:
         return
-    if level == 0 :
+    if level == 0:
         return
     for limb in branch:
-        #for i in range(indent):
+        # for i in range(indent):
         #    print("    ",end="")
         print(
             textwrap.indent(
@@ -61,12 +61,14 @@ def recursive(branch, level=1, indent_size=4, indent=0):
                 "".join([" "]*indent_size*indent)
             )
         )
-        recursive(limb,level-1,indent_size,indent+1)
+        recursive(limb, level-1, indent_size, indent+1)
 
-if __name__ == "__main__":
-    main()
 
 def main():
     tree = ET.parse(args.input_file)
     root = tree.getroot()
     recursive(root, args.depth, args.indent_size)
+
+
+if __name__ == "__main__":
+    main()
