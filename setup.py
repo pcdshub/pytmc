06ed3a09ab4b401a028a73af4110639037244587
K Lauer <klauer@slac.stanford.edu>
2019-03-28 11:41:00 -0700

    BLD: include templates in installed package

--- setup.py
+++ setup.py
@@ -16,5 +16,8 @@ setup(
             'xmltranslate = pytmc.bin.xmltranslate:main',
         ]
     },
-    include_package_data = True,
+    package_data={
+      'pytmc': ['templates/*'],
+    },
+    include_package_data=True,
 )
