9d60ca2289b1680375b80d5fd626d92f9f3873c7
K Lauer <klauer@slac.stanford.edu>
2019-04-15 13:52:18 -0700

    MNT: add __all__ to pytmc.__init__

--- pytmc/__init__.py
+++ pytmc/__init__.py
@@ -4,6 +4,16 @@ from ._version import get_versions  # noqa
 from .xml_obj import Symbol, DataType, SubItem  # noqa
 from .xml_collector import TmcFile  # noqa
 
+
 logger = logging.getLogger(__name__)
 __version__ = get_versions()['version']
 del get_versions
+
+
+__all__ = [
+    'DataType',
+    'SubItem',
+    'Symbol',
+    'TmcFile',
+    'logger',
+]
