43527d1f8dfe6bcdbfc95db4948dd4b6b7abbfde
teddyrendahl <trendahl@slac.stanford.edu>
2019-04-30 14:25:38 -0700

    TST: Basic testing for EPICSRecord templating

---
+++ tests/test_record.py
@@ -0,0 +1,17 @@
+from pytmc.record import EPICSRecord
+
+
+def test_epics_record_render():
+    kwargs = {'pvname': 'Tst:pv',
+              'record_type': 'ai',
+              'fields': {'ZNAM': 'Out',
+                         'ONAM': 'In'}}
+
+    ec = EPICSRecord(**kwargs)
+    record = ec.render_template()
+    print(record)  # For debug purposes
+    assert kwargs['pvname'] in record
+    assert kwargs['record_type'] in record
+    for key, value in kwargs['fields'].items():
+        assert key in record
+        assert value in record
