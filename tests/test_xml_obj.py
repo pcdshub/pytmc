df48a66141ffd0163a1f8650390eb2b21f1288ff
K Lauer <klauer@slac.stanford.edu>
2019-07-01 10:23:06 -0700

    TST: fix xml_obj tests

--- tests/test_xml_obj.py
+++ tests/test_xml_obj.py
@@ -48,10 +48,7 @@ def test_Configuration_config_lines(leaf_bool_pragma_string_w_semicolon,
 
 def test_Configuration_neaten_field(leaf_bool_pragma_string):
     cfg = Configuration(leaf_bool_pragma_string)
-    cfg_lines = cfg._config_lines()
-    result = cfg._neaten_field(cfg_lines[2]['tag'])
-
-    assert result == {'f_name': 'ZNAM', 'f_set': 'SINGLE'}
+    assert cfg.config_lines[2]['tag'] == {'f_name': 'ZNAM', 'f_set': 'SINGLE'}
 
 
 def test_Configuration_formatted_config_lines(leaf_bool_pragma_string):
@@ -77,9 +74,8 @@ def test_Configuration_formatted_config_lines(leaf_bool_pragma_string):
 
 def test_Configuration_config_by_name(leaf_bool_pragma_string):
     cfg = Configuration(leaf_bool_pragma_string)
-    result = cfg._config_by_name()
-    assert result == [
-        [
+    assert cfg.configs == {
+        'TEST:MAIN:NEW_VAR_OUT': [
             {'title': 'pv', 'tag': 'TEST:MAIN:NEW_VAR_OUT'},
             {'title': 'type', 'tag': 'bo'},
             {'title': 'field', 'tag': {'f_name': 'ZNAM', 'f_set': 'SINGLE'}},
@@ -89,7 +85,7 @@ def test_Configuration_config_by_name(leaf_bool_pragma_string):
             {'title': 'io', 'tag': 'o'},
             {'title': 'init', 'tag': 'True'},
         ],
-        [
+        'TEST:MAIN:NEW_VAR_IN': [
             {'title': 'pv', 'tag': 'TEST:MAIN:NEW_VAR_IN'},
             {'title': 'type', 'tag': 'bi'},
             {'title': 'field', 'tag': {'f_name': 'ZNAM', 'f_set': 'SINGLE'}},
@@ -98,31 +94,15 @@ def test_Configuration_config_by_name(leaf_bool_pragma_string):
             {'title': 'str', 'tag': '%d'},
             {'title': 'io', 'tag': 'i'},
         ]
-    ]
-
-
-def test_Configuration_select_config_by_name(leaf_bool_pragma_string):
-    cfg = Configuration(leaf_bool_pragma_string)
-    result = cfg._select_config_by_name('TEST:MAIN:NEW_VAR_OUT')
-    assert result == [
-        {'title': 'pv', 'tag': 'TEST:MAIN:NEW_VAR_OUT'},
-        {'title': 'type', 'tag': 'bo'},
-        {'title': 'field', 'tag': {'f_name': 'ZNAM', 'f_set': 'SINGLE'}},
-        {'title': 'field', 'tag': {'f_name': 'ONAM', 'f_set': 'MULTI'}},
-        {'title': 'field', 'tag': {'f_name': 'SCAN', 'f_set': '1 second'}},
-        {'title': 'str', 'tag': '%d'},
-        {'title': 'io', 'tag': 'o'},
-        {'title': 'init', 'tag': 'True'},
-    ]
+    }
 
 
 def test_Configuration_config_names(leaf_bool_pragma_string):
     cfg = Configuration(leaf_bool_pragma_string)
-    result = cfg.config_names()
-    assert result == [
+    assert set(cfg.configs) == {
         "TEST:MAIN:NEW_VAR_OUT",
         "TEST:MAIN:NEW_VAR_IN"
-    ]
+    }
 
 
 def test_Configuration_fix_to_config_name(leaf_bool_pragma_string):
@@ -136,26 +116,6 @@ def test_Configuration_fix_to_config_name(leaf_bool_pragma_string):
         {'title': 'str', 'tag': '%d'},
         {'title': 'io', 'tag': 'o'},
         {'title': 'init', 'tag': 'True'},
-    ]
-
-
-def test_Configuration_add_config_line(branch_bool_pragma_string):
-    cfg = Configuration(branch_bool_pragma_string)
-    cfg.add_config_line('pv', 'THIRD')
-    assert cfg.config == [
-        {'title': 'pv', 'tag': 'FIRST'},
-        {'title': 'pv', 'tag': 'SECOND'},
-        {'title': 'pv', 'tag': 'THIRD'},
-    ]
-
-
-def test_Configuration_add_config_field(branch_bool_pragma_string):
-    cfg = Configuration(branch_bool_pragma_string)
-    cfg.add_config_field('ABC', 'XYZ', 1)
-    assert cfg.config == [
-        {'title': 'pv', 'tag': 'FIRST'},
-        {'title': 'field', 'tag': {'f_name': 'ABC', 'f_set': '"XYZ"'}},
-        {'title': 'pv', 'tag': 'SECOND'},
     ]
 
 
