76bd5986f2c45289fc86b10b9982c5b5ad701047
Zachary Lentz <zlentz@slac.stanford.edu>
2019-09-17 16:14:51 -0700

    TST: restore test broken by swapping FB_MotionStage for DUT_MotionStage

--- tests/test_project.py
+++ tests/test_project.py
@@ -34,14 +34,10 @@ def test_smoke_ams_id(project):
 
 
 def test_fb_motionstage_linking(project):
-    for inst in project.find(parser.Symbol_FB_MotionStage):
+    for inst in project.find(parser.Symbol_DUT_MotionStage):
         pprint.pprint(inst)
         print('Program name', inst.program_name)
         print('Motor name', inst.motor_name)
-        print('POU', inst.pou)
-        inst.pou.variables  # smoke testing
-        print('Call block', inst.call_block)
-        print('Linked to', inst.linked_to)
         print('NC to PLC link', inst.nc_to_plc_link)
 
         nc_axis = inst.nc_axis
