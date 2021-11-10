bb02632fba8cd4c81805e15302fd3c2fc13f1e96
Ken Lauer <klauer@slac.stanford.edu>
2021-11-09 16:18:28 -0800

    FIX: lines_between needs to find the start_marker first

--- pytmc/code.py
+++ pytmc/code.py
@@ -1,10 +1,9 @@
 '''
 Code parsing-related utilities
 '''
-import logging
 import collections
+import logging
 import re
-
 
 logger = logging.getLogger(__name__)
 
@@ -70,10 +69,11 @@ def lines_between(text, start_marker, end_marker, *, include_blank=False):
     for line in text.splitlines():
         if line.strip().lower() == start_marker:
             found_start = True
-        elif line.strip().lower() == end_marker:
-            break
-        elif found_start and (line.strip() or include_blank):
-            yield line
+        elif found_start:
+            if line.strip().lower() == end_marker:
+                break
+            elif line.strip() or include_blank:
+                yield line
 
 
 def variables_from_declaration(declaration, *, start_marker='var'):
